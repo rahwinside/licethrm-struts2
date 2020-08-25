344da9545fcf4a212c796561bf77f8e926053c66
Rahuls-iMac <rahwinside@gmail.com>
2020-08-26 01:43:47 +0530

    First Commit

---
+++ src/java/HRM/AddEmployeeAction.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HRM;
+import com.opensymphony.xwork2.Action;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ *
+ * @author rah
+ */
+public class AddEmployeeAction implements Action {
+    String employeeID;
+    String employeeName;
+    String employeeDept;
+    String employeePhone;
+    String employeeEmail;
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeDept() {
+        return employeeDept;
+    }
+
+    public void setEmployeeDept(String employeeDept) {
+        this.employeeDept = employeeDept;
+    }
+
+    public String getEmployeePhone() {
+        return employeePhone;
+    }
+
+    public void setEmployeePhone(String employeePhone) {
+        this.employeePhone = employeePhone;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+    
+    @Override
+    public String execute() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/employeestruts", "root", "root");
+ 
+        try {
+            try {
+                Statement st = con.createStatement();
+                int value = st
+                        .executeUpdate("INSERT INTO employee "
+                                + "VALUES('"
+                                + employeeID
+                                + "','"
+                                + employeeName
+                                + "','"
+                                + employeeDept
+                                + "','"
+                                + employeePhone
+                                + "','"
+                                + employeeEmail + "')");
+                System.out.println("1 row affected" + value);
+                con.close();
+                return SUCCESS;
+            } catch (SQLException ex) {
+                System.out.println("SQL statement is not executed!" + ex);
+                return ERROR;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ERROR;
+        }
+    }
+}
